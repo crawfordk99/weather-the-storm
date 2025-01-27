aa6dda14e7afa0052a06d6ca8e3343e783b76ccb
crawfordk99 <keithacrawfordjr@gmail.com>
2025-01-27 14:42:56 -0700

    Separated key from link

--- src/weather/ParseWeatherResponse.java
+++ src/weather/ParseWeatherResponse.java
@@ -1,56 +1,52 @@
 package weather;
 
 public class ParseWeatherResponse {
-	
-	// Sets a member instance of the request class in order to inherit it's getresponse function
+
+	// Sets a member instance of the request class in order to inherit it's
+	// getresponse function
 	private final Requests request;
-	
-	// Sets the request instance 
+	private String _key = "ef3b958c26084d69a2d222506251801";
+
+	// Sets the request instance
 	public ParseWeatherResponse(String city) {
-		this.request = new Requests("http://api.weatherapi.com/v1/alerts.json?key=ef3b958c26084d69a2d222506251801&q=" + city);
+		this.request = new Requests("http://api.weatherapi.com/v1/alerts.json?key=" + _key + "&q=" + city);
 	}
-	
-	
+
 	// Parses the json response from Requests, and returns string array of alerts
 	public String weatherAlert() {
 		String alertArray = "";
 		try {
-			
+
 			// Use the requests class to get API respone
 			String response = request.GetResponse();
-			
+
 			// Make sure the responses aren't empty before splitting
 			if (!response.split("\"alerts\":")[1].isEmpty()) {
-				
+
 				// Extract "alerts" section
-				String alertsContent = response.split("\"alerts\":")[1]; 
-				
+				String alertsContent = response.split("\"alerts\":")[1];
+
 				if (!alertsContent.split("\"alert\":")[1].isEmpty()) {
-					
-				// Extract "alert" array	
-				alertArray = alertsContent.split("\"alert\":")[1];   
+
+					// Extract "alert" array
+					alertArray = alertsContent.split("\"alert\":")[1];
 				}
-				
+
 				// If alertsContent is empty, don't store response to write
 				else {
-				System.out.println("There are no weather alerts for this location currently.");
+					System.out.println("There are no weather alerts for this location currently.");
 				}
-			}
-			else {
+			} else {
 				System.out.println("Response is empty");
 			}
-					
-				
-		}
-		catch (Exception e){
+
+		} catch (Exception e) {
 			System.out.println("An error has occurred");
 			e.printStackTrace();
 		}
-		
-        // return the alerts content
-        return alertArray;
+
+		// return the alerts content
+		return alertArray;
 	}
-	
-	
 
 }
